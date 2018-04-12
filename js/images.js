03197c93118409da9a48793720e36d28ee6710c7
SrDuds <30565729+SrDuds@users.noreply.github.com>
2018-04-12 12:25:27 -0300

    img

--- js/images.js
+++ js/images.js
@@ -1,9 +1,9 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  var randomNumber = Math.floor((Math.random() * 7) + 1);
+  var randomNumber = Math.floor((Math.random() * 8) + 1);
   var randomBackground = randomNumber + ".png";
 
-  var id = ['#E38BAC', '#1DF3FF', '#ffffff', '#ffffff', '#E3CBE0', '#D7CBC1', '#FAEFD7'];
+  var id = ['#E38BAC', '#1DF3FF', '#ffffff', '#ffffff', '#E3CBE0', '#D7CBC1', '#FAEFD7', '#AAB5B2'];
   var randomColor = Math.floor(randomNumber - 1);
 
   console.log(randomNumber);
